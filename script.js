f43fd2719d6395e653864eb845cc05f959ea38ad
szaizen <38065394+szaizen@users.noreply.github.com>
2019-04-01 21:42:49 +0900

    問題１完成

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 jQuery(document).ready(function(){
- //jQueryで実行する内容
- 	console.log('test');
+
+	//problem1
+	var answer1 = 0;
+	for( var i = 1; i < 1000; i++ ) {
+		if ( i % 3 === 0 || i % 5 === 0 ) {
+			answer1 += i;
+		}
+	}
+	$('.answer1').text(answer1);
+
 });
